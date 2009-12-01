a2dce75afdb7362789e2cdac00eea3dc66984697
Tyler Green <jorge@famicom.local>
2009-12-01 16:35:41 -0600

    cleaned up code and fixed bugs

--- src/bot1.clj
+++ src/bot1.clj
@@ -10,6 +10,12 @@
      [ch.idsia.utils MathX])
     )
 
+(defmacro defs [& bindings]
+  `(do ~@(map #(cons 'def %) (partition 2 bindings))))
+
+(defmacro unless [p & body]
+  `(when (not ~p) ~@body))
+
 (defn -reset [this])
 
 (def toggle (atom true))
@@ -20,66 +26,64 @@
 
 (def prev-move (atom []))
 
-(defn jump-b-down? [cpad]
-  (nth @cpad 3))
+(defs
+  left   0
+  right  1 
+  down   2
+  jump   3
+  speed  4
+  up     5
+  pause 6 )
 
-(defn button-mash [obs]
-  (let [moves (make-array (. Boolean TYPE) 5)]
-    (aset moves 4 true)
+(defn press [button controller]
+  (aset controller button true))
+
+(defn release [button controller]
+  (aset controller button false))
+
+(defn pressing? [button controller]
+  (nth @controller button))
+
+(defn button-masher [obs]
+  (let [control (make-array (. Boolean TYPE) 5)]
+    (press right control)
     (when (get-toggle)
       (do
-	(aset moves 1 true)
+	(press speed control)
 	(when (. obs isMarioOnGround)
-	  (aset moves 3 true))))
-    (reset! prev-move (seq moves))
-    moves))
+	  (press jump control))))
+    control))
 
-(defn release-jump-b [control]
-  (aset control 3 false))
-(defn press-jump-b [control]
-  (aset control 3 true))
-
+; has completed a whole level! Twice!  Unbelievable
 (defn high-jumper [obs]
-  (let [moves (make-array (. Boolean TYPE) 5)]
-    (aset moves 1 true)
+  (let [control (make-array (. Boolean TYPE) 5)]
+    (press right control)
     (when (get-toggle)
-      (aset moves 4 true))
-    (if (and (. obs isMarioOnGround) (jump-b-down? prev-move))
-      (release-jump-b moves)
-      (press-jump-b moves))
-    (reset! prev-move (seq moves))
-    moves))
+      (press speed control))
+    (unless (and (. obs isMarioOnGround) (pressing? jump prev-move))
+      (press jump control))
+    (reset! prev-move (seq control))
+    control))
 
 (defn -getAction [this obs]
-;  (button-mash obs)
+;  (button-masher obs)
   (high-jumper obs))
 
-(defn -getName [this] "clojure-bot")
-
-;;     public static final int KEY_LEFT = 0;
-;;     public static final int KEY_RIGHT = 1;
-;;     public static final int KEY_DOWN = 2;
-;;     public static final int KEY_JUMP = 3;
-;;     public static final int KEY_SPEED = 4;
-;;     public static final int KEY_UP = 5;
-;;     public static final int KEY_PAUSE = 6;
-;;     public static final int KEY_DUMP_CURRENT_WORLD = 7;
-;;     public static final int KEY_LIFE_UP = 8;
-;;     public static final int KEY_WIN = 9;
-
+(defn -getName [this] "Clojure Bot")
+  
 ; to compile bot:
 ; cd ~/cs/myclojure/marioai
 
 ; Start clojure with proper classpath
-; java -cp /Users/jorge/cs/marioai/classes:/Users/jorge/cs/clojure/clojure.jar:bin:src clojure.main $1
+; java -cp /Users/jorge/cs/marioai/classes:/Users/jorge/cs/clojure/clojure-core/clojure.jar:bin:src clojure.main $1
 
-; (set! *compile-path* "/Users/jorge/cs/myclojure/marioai/bin")
+; (set! *compile-path* "/Users/jorge/cs/clojure/marioai/bin")
 
 ; (compile 'bot1)
 
 
 ;  To run compiled bot: (in myclojure/marioai)
-; java -cp bin:/Users/jorge/cs/marioai/classes:/Users/jorge/cs/clojure/clojure.jar ch.idsia.scenarios.Play bot1
+; java -cp bin:/Users/jorge/cs/marioai/classes:/Users/jorge/cs/clojure/clojure-core/clojure.jar ch.idsia.scenarios.Play bot1
 
 ; Be sure to put CLASSES in cp not SRC!!
 
